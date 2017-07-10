ac64f8cf328d78136d8fb870e76f8ce3237e325d
tdale9010 <tdale9010@users.noreply.github.com>
2017-07-09 21:50:26 -0500

    Adding more stored procedures

--- Create Stored Procedures.sql
+++ Create Stored Procedures.sql
@@ -1,4 +1,118 @@
 DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Actor`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from actors where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_City`(IN SearchTerm Varchar(254))
+BEGIN
+	select distinct City from locations where City like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Country`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from countries where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Director`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from directors where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Genre`(IN SearchTerm Varchar(254))
+BEGIN
+	select Genres from genres where Genres like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Region`(IN SearchTerm Varchar(254))
+BEGIN
+	select distinct Region from locations where Region like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Tag`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from tags where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Film_Delete`(IN UserIDIn int, IN MovieIDIn Int)
+BEGIN
+	Delete from favoritefilms where userID=userIDIn and MovieID=MovieIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Films`(IN UserIDIn int)
+BEGIN
+	Select m.Title,m.rating,m.Photo,m.Year from favoriteMovies fm
+    inner join movies m on fm.MovieID=m.movieid
+    where fm.userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Films_Insert`(IN UserIDIn int, IN MovieIDIn int)
+BEGIN
+	Insert into favoritefilms values (userIDIn, MovieIDIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre`(IN UserIDIn int)
+BEGIN
+	Select GenreName from favoriteGenres where userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre_Delete`(IN UserIDIn int, IN GenreIn Varchar(254))
+BEGIN
+	Delete from favoriteGenres where userID=userIDIn and GenreName=GenreIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre_Insert`(IN UserIDIn int, IN GenreIn Varchar(254))
+BEGIN
+	Insert into favoriteGenres values (userIDIn, GenreIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag`(IN UserIDIn int)
+BEGIN
+	Select TagName from favoriteTags where userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag_Delete`(IN UserIDIn int, IN TagIn Varchar(254))
+BEGIN
+	Delete from favoriteTags where userID=userIDIn and TagName=TagIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag_Insert`(IN UserIDIn int, IN TagIn Varchar(254))
+BEGIN
+	Insert into favoriteTags values (userIDIn, TagIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Movie_Info`(IN MovieID INT)
 BEGIN
 	SELECT distinct m.title as 'Title',m.rating as 'Rating',d.Name as 'Director',
@@ -56,5 +170,55 @@ BEGIN
     
     
     ;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info`(IN UserIDIn int)
+BEGIN
+	Select * from users where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Create_All`(IN UserIDIn int,IN NameIn Varchar(254)
+, IN PasswordIn VarChar(254), IN RatingIn Decimal(8,0))
+BEGIN
+	Insert into users values (UserIDIn, NameIn,PasswordIn,RatingIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Delete_All`(IN UserIDIn int)
+BEGIN
+	Delete from users where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Next_ID`()
+BEGIN
+	Select Max(ID)+1 from users;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Name`(IN UserIDIn int,IN NameIn varchar(254))
+BEGIN
+	Update users set name=NameIn where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Password`(IN UserIDIn int,IN PasswordIn varchar(254))
+BEGIN
+	Update users set passwordhash=PasswordIn where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Rating`(IN UserIDIn int,IN RatingIn Decimal(8,0))
+BEGIN
+	Update users set minrating=ratingIn where ID=userIDin;
 END$$
 DELIMITER ;
