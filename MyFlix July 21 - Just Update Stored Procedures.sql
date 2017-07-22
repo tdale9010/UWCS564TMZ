5a29adb9a4ba6a0b3fa42b31ab2e67fa2791deb5
tdale9010 <tdale9010@users.noreply.github.com>
2017-07-21 22:57:58 -0500

    Adding the most recent database makeup here.

---
+++ MyFlix July 21 - Just Update Stored Procedures.sql
@@ -0,0 +1,328 @@
+DROP PROCEDURE `dbo`.`Add_Tag`;
+DROP PROCEDURE `dbo`.`Autocomplete_Actor`;
+DROP PROCEDURE `dbo`.`Autocomplete_City`;
+DROP PROCEDURE `dbo`.`Autocomplete_Country`;
+DROP PROCEDURE `dbo`.`Autocomplete_Director`;
+DROP PROCEDURE `dbo`.`Autocomplete_Genre`;
+DROP PROCEDURE `dbo`.`Autocomplete_Region`;
+DROP PROCEDURE `dbo`.`Autocomplete_Tag`;
+DROP PROCEDURE `dbo`.`Favorite_Film_Delete`;
+DROP PROCEDURE `dbo`.`Favorite_Films`;
+DROP PROCEDURE `dbo`.`Favorite_Films_Insert`;
+DROP PROCEDURE `dbo`.`Favorite_Genre`;
+DROP PROCEDURE `dbo`.`Favorite_Genre_Delete`;
+DROP PROCEDURE `dbo`.`Favorite_Genre_Insert`;
+DROP PROCEDURE `dbo`.`Favorite_Tag`;
+DROP PROCEDURE `dbo`.`Favorite_Tag_Delete`;
+DROP PROCEDURE `dbo`.`Favorite_Tag_Insert`;
+DROP PROCEDURE `dbo`.`Movie_Info`;
+DROP PROCEDURE `dbo`.`Search_Results`;
+DROP PROCEDURE `dbo`.`Search_Results_Full_Info`;
+DROP PROCEDURE `dbo`.`Tag_Movie`;
+DROP PROCEDURE `dbo`.`Tag_Movie_Remove`;
+DROP PROCEDURE `dbo`.`User_Info`;
+DROP PROCEDURE `dbo`.`User_Info_Create_All`;
+DROP PROCEDURE `dbo`.`User_Info_Delete_All`;
+DROP PROCEDURE `dbo`.`User_Info_Next_ID`;
+DROP PROCEDURE `dbo`.`User_Info_Update_Name`;
+DROP PROCEDURE `dbo`.`User_Info_Update_Password`;
+DROP PROCEDURE `dbo`.`User_Info_Update_Rating`;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Add_Tag`(In TagIn Varchar(254))
+BEGIN
+	Insert into tags values (TagIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Actor`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from actors where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_City`(IN SearchTerm Varchar(254))
+BEGIN
+	select distinct City from locations where City like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Country`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from countries where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Director`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from directors where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Genre`(IN SearchTerm Varchar(254))
+BEGIN
+	select Genres from genres where Genres like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Region`(IN SearchTerm Varchar(254))
+BEGIN
+	select distinct Region from locations where Region like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Autocomplete_Tag`(IN SearchTerm Varchar(254))
+BEGIN
+	select Name from tags where Name like concat(SearchTerm,'%');
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Film_Delete`(IN UserIDIn int, IN MovieIDIn Int)
+BEGIN
+	Delete from favoritefilms where userID=userIDIn and MovieID=MovieIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Films`(IN UserIDIn int)
+BEGIN
+	Select m.Title,m.rating,m.Photo,m.Year from favoriteMovies fm
+    inner join movies m on fm.MovieID=m.movieid
+    where fm.userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Films_Insert`(IN UserIDIn int, IN MovieIDIn int)
+BEGIN
+	Insert into favoritefilms values (userIDIn, MovieIDIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre`(IN UserIDIn int)
+BEGIN
+	Select GenreName from favoriteGenres where userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre_Delete`(IN UserIDIn int, IN GenreIn Varchar(254))
+BEGIN
+	Delete from favoriteGenres where userID=userIDIn and GenreName=GenreIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Genre_Insert`(IN UserIDIn int, IN GenreIn Varchar(254))
+BEGIN
+	Insert into favoriteGenres values (userIDIn, GenreIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag`(IN UserIDIn int)
+BEGIN
+	Select TagName from favoriteTags where userID=UserIDIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag_Delete`(IN UserIDIn int, IN TagIn Varchar(254))
+BEGIN
+	Delete from favoriteTags where userID=userIDIn and TagName=TagIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Favorite_Tag_Insert`(IN UserIDIn int, IN TagIn Varchar(254))
+BEGIN
+	Insert into favoriteTags values (userIDIn, TagIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Movie_Info`(IN MovieID INT)
+BEGIN
+	SELECT distinct m.title as 'Title',m.rating as 'Rating',d.Name as 'Director',
+    (
+		select group_concat( distinct mg.GenreName separator '|' ) 
+		from dbo.movieofgenre mg 
+		where mg.movieid=MovieID) as 'Genres',
+    (
+		select group_concat( distinct a.name separator '|' ) 
+		from dbo.starring s 
+		inner join dbo.actors a on a.actorid=s.actorid
+		where s.movieid=MovieID
+    ) as 'Actors',
+    (
+		select group_concat( distinct concat(fi.city,', ',fi.region) separator '|' ) 
+		from dbo.filmedin fi 
+		where fi.movieid=MovieID
+    ) as 'Film Locations',
+    m.Countryofproduction as 'Country of Origin',
+    (
+		select group_concat( distinct mt.TagName separator '|' ) 
+		from dbo.movietaggedas mt 
+		where mt.movieid=MovieID
+    ) as 'Tags',
+    m.Photo as 'Photo'
+    
+    from dbo.movies m
+    Left outer join dbo.directors d on m.DirectorID=d.DirectorID
+    where m.movieid=MovieID;
+    
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Search_Results`(IN TitleIn VARCHAR(254), IN RatingIn Decimal(5,4),
+ IN DirectorIn VARCHAR(254), IN GenreIn VARCHAR(254), IN ActorIn Varchar(254), IN FilmLocationIn Varchar(254),
+ IN CountryIn VARCHAR(254), IN TagIn Varchar(254))
+BEGIN
+	SELECT distinct m.MovieID from dbo.movies m
+    Left outer join dbo.directors d on m.DirectorID=d.DirectorID
+    left outer join dbo.movieofgenre mg on mg.movieid=m.movieid
+    left outer join dbo.starring s on s.MovieID=m.movieID
+    left outer join dbo.actors a on s.actorid=a.actorid
+    left outer join dbo.filmedIn fi on fi.movieID=m.movieID
+    left outer join dbo.movietaggedas mt on mt.movieid=m.movieid
+    WHERE (titleIn is null or m.title like Concat('%',titleIn,'%'))
+    AND (ratingIn is null or m.rating >= RatingIn)
+    AND (DirectorIn is null or d.name like Concat('%',DirectorIn,'%'))
+    AND (GenreIn is null or mg.GenreName like concat('%',GenreIn,'%'))
+    AND (ActorIn is null or a.name like concat('%',ActorIn,'%'))
+    AND (ActorIn is null or a.name like concat('%',ActorIn,'%'))
+    AND (FilmLocationIn is null or fi.region like concat('%',FilmLocationIn,'%') or fi.city like concat('%',FilmLocationIn,'%'))
+    AND (CountryIn is null or m.countryofproduction like concat('%',CountryIn,'%'))
+    AND (TagIn is null or mt.TagName like concat('%',TagIn,'%'))
+    
+    
+    ;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Search_Results_Full_Info`(IN TitleIn VARCHAR(254), IN RatingIn Decimal(5,4),
+ IN DirectorIn VARCHAR(254), IN GenreIn VARCHAR(254), IN ActorIn Varchar(254), IN FilmLocationIn Varchar(254),
+ IN CountryIn VARCHAR(254), IN TagIn Varchar(254))
+BEGIN
+		SELECT distinct m.title as 'Title',m.rating as 'Rating',d.Name as 'Director',
+    (
+		select group_concat( distinct mg.GenreName separator '|' ) 
+		from dbo.movieofgenre mg 
+		where mg.movieid=MovieID) as 'Genres',
+    (
+		select group_concat( distinct a.name separator '|' ) 
+		from dbo.starring s 
+		inner join dbo.actors a on a.actorid=s.actorid
+		where s.movieid=MovieID
+    ) as 'Actors',
+    (
+		select group_concat( distinct concat(fi.city,', ',fi.region) separator '|' ) 
+		from dbo.filmedin fi 
+		where fi.movieid=MovieID
+    ) as 'Film Locations',
+    m.Countryofproduction as 'Country of Origin',
+    (
+		select group_concat( distinct mt.TagName separator '|' ) 
+		from dbo.movietaggedas mt 
+		where mt.movieid=MovieID
+    ) as 'Tags',
+    m.Photo as 'Photo'
+    
+    from dbo.movies m
+    Left outer join dbo.directors d on m.DirectorID=d.DirectorID
+    where m.movieid in 
+    (
+    SELECT distinct m.MovieID from dbo.movies m
+    Left outer join dbo.directors d on m.DirectorID=d.DirectorID
+    left outer join dbo.movieofgenre mg on mg.movieid=m.movieid
+    left outer join dbo.starring s on s.MovieID=m.movieID
+    left outer join dbo.actors a on s.actorid=a.actorid
+    left outer join dbo.filmedIn fi on fi.movieID=m.movieID
+    left outer join dbo.movietaggedas mt on mt.movieid=m.movieid
+    WHERE (titleIn is null or m.title like Concat('%',titleIn,'%'))
+    AND (ratingIn is null or m.rating >= RatingIn)
+    AND (DirectorIn is null or d.name like Concat('%',DirectorIn,'%'))
+    AND (GenreIn is null or mg.GenreName like concat('%',GenreIn,'%'))
+    AND (ActorIn is null or a.name like concat('%',ActorIn,'%'))
+    AND (ActorIn is null or a.name like concat('%',ActorIn,'%'))
+    AND (FilmLocationIn is null or fi.region like concat('%',FilmLocationIn,'%') or fi.city like concat('%',FilmLocationIn,'%'))
+    AND (CountryIn is null or m.countryofproduction like concat('%',CountryIn,'%'))
+    AND (TagIn is null or mt.TagName like concat('%',TagIn,'%')))
+    ;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Tag_Movie`(IN MovieIDIN int, In TagIn Varchar(254))
+BEGIN
+	Insert into movietaggedas values (MovieIDIN,TagIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Tag_Movie_Remove`(IN MovieIDIN int, In TagIn Varchar(254))
+BEGIN
+	Delete from movietaggedas where movieID=MovieIDIN and TagName=TagIn;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info`(IN UserIDIn int)
+BEGIN
+	Select * from users where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Create_All`(IN UserIDIn int,IN NameIn Varchar(254)
+, IN PasswordIn VarChar(254), IN RatingIn Decimal(8,0))
+BEGIN
+	Insert into users values (UserIDIn, NameIn,PasswordIn,RatingIn);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Delete_All`(IN UserIDIn int)
+BEGIN
+	Delete from users where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Next_ID`()
+BEGIN
+	Select Max(ID)+1 from users;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Name`(IN UserIDIn int,IN NameIn varchar(254))
+BEGIN
+	Update users set name=NameIn where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Password`(IN UserIDIn int,IN PasswordIn varchar(254))
+BEGIN
+	Update users set passwordhash=PasswordIn where ID=userIDin;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Info_Update_Rating`(IN UserIDIn int,IN RatingIn Decimal(8,0))
+BEGIN
+	Update users set minrating=ratingIn where ID=userIDin;
+END$$
+DELIMITER ;
