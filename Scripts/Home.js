cd06fece0451ec6b2e3fb23adba6eda670e7df4c
zhathawa <zach.hathaway48@gmail.com>
2017-07-30 22:06:07 -0500

    Add files via upload

---
+++ Scripts/Home.js
@@ -0,0 +1,124 @@
+﻿$(document).ready(function (e)
+{
+	$("#userNameHolder").hide();
+	$("#saveUserName").prop('disabled', true);
+	$("#ratingHolder").hide();
+
+	$("#updateUserName").click(function () {
+		$("#userNameHolder").show();
+	});
+
+	$("#updateRating").click(function () {
+		$("#ratingHolder").show();
+	});
+
+	$("#newUserName").keydown(function ()
+	{
+		var username = $("#newUserName").val();
+		if (validateEmail(username)) {
+			$("#saveUserName").prop('disabled', false);
+		} else {
+			$("#saveUserName").prop('disabled', true);
+		}
+	});
+
+	$("#saveUserName").click(function () {
+		var username = $("#newUserName").val();
+		var password = $("#password").val();
+
+		$.ajax({
+			type: "POST",
+			url: "UpdateUserName?userName=" + username + "&password=" + password,   // This is terrible, but I don't have time to do it the right way
+			success: function (data) {
+				location.reload();
+			}
+		});
+	});
+
+	$("#saveRating").click(function () {
+		var rating = $("#newRating").val()
+
+		$.ajax({
+			type: "POST",
+			url: "UpdateRating?rating=" + rating,
+			success: function (data) {
+				location.reload();
+			}
+		});
+	});
+
+	$(".removeFilm").click(function () {
+		var movie = $(this).val();
+		var model = {};
+		model.ID = movie;
+
+		$.ajax({
+			type: "POST",
+			url: "RemoveFavoriteMovie",
+			data: model,
+			dataType: "json",
+			success: function (data)
+			{
+				var selector = "[value^='" + $.trim(model.ID) + "'";
+				$(selector).fadeOut();
+			},
+			error: function ()
+			{
+				alert('error handing here');
+			}
+		});
+	});
+
+	$(".removeGenre").click(function ()
+	{
+		var genre = $(this).val();
+		var model = {};
+		model.Name = genre;
+
+		$.ajax({
+			type: "POST",
+			url: "RemoveFavoriteGenre",
+			data: model,
+			dataType: "json",
+			success: function (data)
+			{
+				var selector = "[value^='" + $.trim(model.Name) + "'";
+				$(selector).fadeOut();
+			},
+			error: function ()
+			{
+				alert('error handing here');
+			}
+		});
+	});
+
+	$(".removeTag").click(function ()
+	{
+		var tag = $(this).val();
+		var model = {};
+		model.Name = tag;
+
+		$.ajax({
+			type: "POST",
+			url: "RemoveFavoriteTag",
+			data: model,
+			dataType: "json",
+			success: function (data)
+			{
+				var selector = "[value^='" + $.trim(model.Name) + "'";
+				$(selector).fadeOut();
+			},
+			error: function ()
+			{
+				alert('error handing here');
+			}
+		});
+	});
+
+});
+
+function validateEmail(email){
+	var re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	var wtf = email.match(re);
+	return email.match(re);
+}
---
+++ Scripts/Search.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function (e)
+{
+	$("#submit").click(function ()
+	{
+		var searchParams = $("#searchForm").serialize();
+		$.ajax({
+			type: "GET",
+			url: "SearchResults",
+			data: searchParams,
+			dataType: "json",
+			success: function (data)
+			{
+				$("#results").empty();
+
+				var table = $("<table></table>");
+				var header = $("<tr><td>Title</td><td>Director</td><td>Rating</td><td>Image</td></tr>");
+				table.append(header);
+
+				for (var i = 0; i < data.Movies.length; i++){
+					var row = $("<tr></tr>");
+					row.append($("<td><a href='/Home/MovieDetails?id=" + data.Movies[i].ID + "'>" + data.Movies[i].Title + "</a></td>"));
+					row.append($("<td>" + data.Movies[i].Director + "</td>"));
+					row.append($("<td>" + data.Movies[i].Rating + "</td>"));
+					row.append($("<td><img src='" + data.Movies[i].ImageURL + "' /></td>"));
+					table.append(row);
+				}
+
+
+				$("#results").append(table);
+				
+			},
+			error: function ()
+			{
+				alert('error handing here');
+			}
+		});
+	});
+});
